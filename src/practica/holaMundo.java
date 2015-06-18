8d0b36330d00413e275740276959fb2a3ee83d4e
josedavid <josedavid@hp.asgard.local>
2015-06-18 16:38:54 -0500

    Elimine las lineas en blanco.

--- src/practica/holaMundo.java
+++ src/practica/holaMundo.java
@@ -4,6 +4,5 @@ public class holaMundo {
 	public static void main(String[]args){
 	System.out.println("Hola mundo, Ésto es una prueba de eGit.");
 	System.out.println("Segundo, Ésto es una prueba de eGit.");
-
 	}
 }
