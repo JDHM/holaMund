b5389112a4905941eb4921e0ac537b74dc0a12be
josedavid <josedavid@hp.asgard.local>
2015-06-18 17:05:59 -0500

    Prueba 3

--- src/practica/holaMundo.java
+++ src/practica/holaMundo.java
@@ -4,5 +4,6 @@ public class holaMundo {
 	public static void main(String[]args){
 	System.out.println("Hola mundo, Ésto es una prueba de eGit.");
 	System.out.println("Segundo, Ésto es una prueba de eGit.");
+	System.out.println("Tercer cambio");
 	}
 }
