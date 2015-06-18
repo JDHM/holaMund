fde28d90e6d4ef737d711c53740487455fb7f83b
josedavid <josedavid@hp.asgard.local>
2015-06-18 16:34:57 -0500

    Otra linea más

--- src/practica/holaMundo.java
+++ src/practica/holaMundo.java
@@ -3,5 +3,7 @@ package practica;
 public class holaMundo {
 	public static void main(String[]args){
 	System.out.println("Hola mundo, Ésto es una prueba de eGit.");
+	System.out.println("Segundo, Ésto es una prueba de eGit.");
+
 	}
 }
